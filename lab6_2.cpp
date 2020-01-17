058cc96a9c7f5002bdd0159aebdd72591eb91a26
lab314-20@labeng.local <lab314-20@labeng.local>
2020-01-17 16:46:37 +0700

    numba 1

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -6,6 +6,7 @@ int main(){
     int id;
     string name,movie,day,word;
     cout << "Fahsai: Sawadee ka...Can you tell me your name?" << endl;
+    cout << "?????:";
     getline(cin,name) ;
     cout << "Fahsai: Wow!!! " << name << " that is a really cool name.\n";
     cout << "Fahsai: I think you are Engineering student. What is your student ID?\n";
